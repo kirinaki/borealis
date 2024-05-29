d484d372887a11512fd9172c541cbc7600c4daed
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-05-29 17:57:08 -0600

    :sparkles: feat: added new config files

---
+++ config/app.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    "debug" => true,
+    "timezone" => 'America/Mexico_City',
+
+    "providers" => [
+        \Core\ServiceProviders\TwigServiceProvider::class,
+        \App\ServiceProviders\ViteServiceProvider::class,
+        \Core\ServiceProviders\ConfigurationServiceProvider::class,
+        \Core\ServiceProviders\PageServiceProvider::class,
+        \Core\ServiceProviders\PostTypeServiceProvider::class,
+        \Core\ServiceProviders\ControllerServiceProvider::class,
+        \App\ServiceProviders\ThemeSupportServiceProvider::class,
+    ],
+
+    "configuration" => [
+        \App\Actions\BootCarbonFields::class,
+        \App\Actions\AddTwigFunctions::class,
+        \App\Actions\SetGeneralContext::class,
+        \App\Actions\AddCommonFieldsAction::class,
+    ],
+
+    "theme_supports" => [
+        \App\ThemeSupport\AutomaticFeedLinksThemeSupport::class,
+        \App\ThemeSupport\Html5ThemeSupport::class,
+        \App\ThemeSupport\MenusThemeSupport::class,
+        \App\ThemeSupport\PostFormatThemeSupport::class,
+        \App\ThemeSupport\PostThumbnailThemeSupport::class,
+        \App\ThemeSupport\TitleTagThemeSupport::class
+    ],
+
+    "pages" => [
+
+    ],
+
+    "post_types" => [
+
+    ],
+
+    "controllers" => [
+        
+    ]
+];
---
+++ config/mail.php
@@ -0,0 +1,7 @@
+<?php
+return [
+	"host"     => SMTP_HOST,
+	"port"     => SMTP_PORT,
+	"username" => SMTP_USERNAME,
+	"password" => SMTP_PASSWORD,
+];
