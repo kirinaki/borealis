38fea80edef45fd76bb58763f60fa1f912cbc8b5
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-06-02 19:27:39 -0600

    :sparkles: feat: added new method

--- core/Borealis.php
+++ core/Borealis.php
@@ -3,8 +3,8 @@
 namespace Core;
 
 
+use Core\Facades\{Config, Facade};
 use DI\Container;
-use Core\Facades\{Facade, Config};
 
 class Borealis
 {
@@ -14,14 +14,27 @@ class Borealis
         $app = new Container([
             "configuration" => [
                 "app" => require_once __DIR__ . "/../config/app.php",
-                "mail" => require_once __DIR__ . "/../config/mail.php"
+                "mail" => require_once __DIR__ . "/../config/mail.php",
             ],
-            "config" => \DI\get(\Core\Support\Config::class)
         ]);
         Facade::setFacadeApplication($app);
+        \Core\Facades\View::register();
+        self::registerConfiguration();
 
         foreach (Config::get("app.providers") as $serviceProvider) {
             (new $serviceProvider($app))->register();
+        }
+
+    }
+
+    static function registerConfiguration(): void
+    {
+        $classes = [
+            \Core\Support\Actions\BootCarbonFields::class,
+            \Core\Support\Actions\SetGeneralContext::class,
+        ];
+        foreach ($classes as $item ) {
+            ( new $item )->register();
         }
     }
 }
