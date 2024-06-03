d7ec432ca1010cf663597237f6c60075b6ba37c7
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-06-02 19:27:27 -0600

    :sparkles: feat: removed unused arrays

--- config/app.php
+++ config/app.php
@@ -6,32 +6,10 @@ return [
     "timezone" => 'America/Mexico_City',
 
     "providers" => [
-        \Core\ServiceProviders\TwigServiceProvider::class,
-        \App\ServiceProviders\ViteServiceProvider::class,
-        \Core\ServiceProviders\ConfigurationServiceProvider::class,
         \Core\ServiceProviders\PageServiceProvider::class,
         \Core\ServiceProviders\PostTypeServiceProvider::class,
         \Core\ServiceProviders\ControllerServiceProvider::class,
         \App\ServiceProviders\ThemeSupportServiceProvider::class,
-    ],
-
-    "aliases" => [
-        "config" => \Core\Facades\Config::class
-    ],
-
-    "configuration" => [
-        \App\Actions\BootCarbonFields::class,
-        \App\Actions\AddTwigFunctions::class,
-        \App\Actions\SetGeneralContext::class,
-    ],
-
-    "theme_supports" => [
-        \App\ThemeSupport\AutomaticFeedLinksThemeSupport::class,
-        \App\ThemeSupport\Html5ThemeSupport::class,
-        \App\ThemeSupport\MenusThemeSupport::class,
-        \App\ThemeSupport\PostFormatThemeSupport::class,
-        \App\ThemeSupport\PostThumbnailThemeSupport::class,
-        \App\ThemeSupport\TitleTagThemeSupport::class
     ],
 
     "pages" => [
