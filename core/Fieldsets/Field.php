8170372c2f2d4ae6c070e267224819561468ffc5
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-06-10 22:34:18 -0600

    :sparkles: feat: renamed labels

--- core/Fieldsets/Field.php
+++ core/Fieldsets/Field.php
@@ -6,26 +6,26 @@ class Field
 {
     public static function image(string $id, string $label): \Carbon_Fields\Field\Field
     {
-        return \Carbon_Fields\Field::make("image", "crb_$id", $label);
+        return \Carbon_Fields\Field::make("image", $id, $label);
     }
 
     public static function text(string $id, string $label): \Carbon_Fields\Field\Field
     {
-        return \Carbon_Fields\Field::make("text", "crb_$id", $label);
+        return \Carbon_Fields\Field::make("text", $id, $label);
     }
 
     public static function textarea(string $id, string $label): \Carbon_Fields\Field\Field
     {
-        return \Carbon_Fields\Field::make("textarea", "crb_$id", $label);
+        return \Carbon_Fields\Field::make("textarea", $id, $label);
     }
 
     public static function richText(string $id, string $label): \Carbon_Fields\Field\Field
     {
-        return \Carbon_Fields\Field::make("rich_text", "crb_$id", $label);
+        return \Carbon_Fields\Field::make("rich_text", $id, $label);
     }
 
     public static function complex(string $id, string $label): \Carbon_Fields\Field\Field
     {
-        return \Carbon_Fields\Field::make("complex", "crb_$id", $label);
+        return \Carbon_Fields\Field::make("complex", $id, $label);
     }
 }
