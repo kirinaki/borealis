da3a0306b0bd7bc398d399af4f71dd384039d319
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-07-13 22:50:28 -0600

    :recycle: refactor: removed unused code

--- app/Actions/Action.php
+++ app/Actions/Action.php
@@ -6,12 +6,4 @@ use \Kirinaki\Framework\Actions\Action as BaseAction;
 
 abstract class Action extends BaseAction
 {
-    protected string $hook = "init";
-
-    public function register(): void
-    {
-        add_action($this->hook, [$this, "handle"]);
-    }
-
-    public abstract function handle(): void;
 }
