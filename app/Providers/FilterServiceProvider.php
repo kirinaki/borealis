7da9f125c69cd95431fed757f65f69b5a887ebc9
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-07-13 23:00:19 -0600

    :sparkles: feat: added new provider

---
+++ app/Providers/FilterServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Providers;
+
+use \Kirinaki\Framework\ServiceProviders\FilterServiceProvider as ServiceProvider;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        
+    }
+}
